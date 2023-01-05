e853980968b5e55a59cf77614f53e991e6947ece
devika <devikamurali08@gmail.com>
2023-01-05 07:04:25 +0530

    end of section 2

---
+++ NZWalks/NzWalks.Api/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalks.Api.Models.Domain;
+
+namespace NzWalks.Api.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
+
---
+++ NZWalks/NzWalks.Api/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,12 @@
+﻿namespace NzWalks.Api.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid Id { get; set; }    
+        public string Code { get; set; }
+
+
+        //Na
+
+    }
+}
